ad833c9b547273744af9b89846776074a10c2809
admin <admin@users.noreply.github.com>
2024-05-22 15:52:46 +0800

    upl base code

---
+++ config.py
@@ -0,0 +1,20 @@
+PATCH_LENGTH = 128      # Patch Length
+PATCH_SIZE = 32       # Patch Size
+
+PATCH_NUM_LAYERS = 9         # Number of layers in the encoder
+CHAR_NUM_LAYERS = 3          # Number of layers in the decoder
+
+# Number of epochs to train for (if early stopping doesn't intervene)
+NUM_EPOCHS = 5  # 32
+LEARNING_RATE = 5e-5            # Learning rate for the optimizer
+# Batch size for patch during training, 0 for full context
+PATCH_SAMPLING_BATCH_SIZE = 0
+LOAD_FROM_CHECKPOINT = True     # Whether to load weights from a checkpoint
+# Whether to share weights between the encoder and decoder
+SHARE_WEIGHTS = False
+WEIGHT_URL = 'https://huggingface.co/MuGeminorum/hoyoGPT/resolve/main/weights.pth'
+WEIGHT_URL_ZH = 'https://www.modelscope.cn/api/v1/models/MuGeminorum/hoyoGPT/repo?Revision=master&FilePath=weights.pth'
+OUTPUT_PATH = './output'
+WEIGHT_PATH = f'{OUTPUT_PATH}/weights.pth'
+LOG_PATH = f'{OUTPUT_PATH}/logs.txt'
+PROMPT_PATH = 'prompt.txt'
---
+++ train.py
@@ -0,0 +1,248 @@
+import os
+import time
+from utils import *
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from modelscope.msdatasets import MsDataset
+from torch.cuda.amp import autocast, GradScaler
+from transformers import GPT2Config, get_scheduler
+
+
+def init():
+    random.seed(42)
+    batch_size = torch.cuda.device_count()
+    patchilizer = Patchilizer()
+
+    patch_config = GPT2Config(
+        num_hidden_layers=PATCH_NUM_LAYERS,
+        max_length=PATCH_LENGTH,
+        max_position_embeddings=PATCH_LENGTH,
+        vocab_size=1,
+    )
+
+    char_config = GPT2Config(
+        num_hidden_layers=CHAR_NUM_LAYERS,
+        max_length=PATCH_SIZE,
+        max_position_embeddings=PATCH_SIZE,
+        vocab_size=128,
+    )
+
+    model = TunesFormer(patch_config, char_config, share_weights=SHARE_WEIGHTS)
+
+    # print parameter number
+    print(
+        f"Parameter Number: {str(sum(p.numel() for p in model.parameters() if p.requires_grad))}"
+    )
+
+    if torch.cuda.device_count() > 1:
+        model = torch.nn.DataParallel(model)
+
+    scaler = GradScaler()
+    is_autocast = True
+    optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+
+    return batch_size, patchilizer, model, scaler, is_autocast, optimizer
+
+
+def collate_batch(batch):
+    input_patches = []
+
+    for input_patch in batch:
+        input_patches.append(input_patch.reshape(-1))
+
+    input_patches = torch.nn.utils.rnn.pad_sequence(
+        input_patches, batch_first=True, padding_value=0
+    )
+
+    return input_patches.to(device)
+
+
+def split_data(data, eval_ratio=0.1):
+    random.shuffle(data)
+    split_idx = int(len(data) * eval_ratio)
+    eval_set = data[:split_idx]
+    train_set = data[split_idx:]
+    return train_set, eval_set
+
+
+def process_one_batch(batch):  # call model with a batch of input
+    input_patches = batch
+    loss = model(input_patches).loss
+    return loss.mean()
+
+
+def train_epoch(model, is_autocast, scaler):  # do one epoch for training
+    tqdm_train_set = tqdm(train_set)
+    total_train_loss = 0
+    iter_idx = 1
+    model.train()
+
+    for batch in tqdm_train_set:
+        try:
+            if is_autocast:
+                with autocast():
+                    loss = process_one_batch(batch)
+
+                if loss == None or torch.isnan(loss).item():
+                    continue
+
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+
+            else:
+                loss = process_one_batch(batch)
+                if loss == None or torch.isnan(loss).item():
+                    continue
+
+                loss.backward()
+                optimizer.step()
+
+        except RuntimeError as exception:
+            if "memory" in str(exception):
+                print(str(exception))
+                if hasattr(torch.cuda, "empty_cache"):
+                    torch.cuda.empty_cache()
+
+                continue
+
+            else:
+                raise exception
+
+        lr_scheduler.step()
+        model.zero_grad(set_to_none=True)
+        total_train_loss += loss.item()
+        tqdm_train_set.set_postfix({"train_loss": total_train_loss / iter_idx})
+        iter_idx += 1
+
+    return total_train_loss / (iter_idx - 1)
+
+
+def eval_epoch(model):  # do one epoch for eval
+    tqdm_eval_set = tqdm(eval_set)
+    total_eval_loss = 0
+    iter_idx = 1
+    model.eval()
+
+    # Evaluate data for one epoch
+    for batch in tqdm_eval_set:
+        with torch.no_grad():
+            loss = process_one_batch(batch)
+            if loss == None or torch.isnan(loss).item():
+                continue
+
+            total_eval_loss += loss.item()
+
+        tqdm_eval_set.set_postfix({"eval_loss": total_eval_loss / iter_idx})
+        iter_idx += 1
+
+    return total_eval_loss / (iter_idx - 1)
+
+
+if __name__ == "__main__":
+    # load data
+    ds = MsDataset.load("monetjoe/EMOPIA_abc", subset_name="default", split="train")
+    emopia = []
+    for item in ds:
+        emopia.append(
+            {
+                "control code": "A:" + item["label"] + "\n" + item["prompt"],
+                "abc notation": item["data"],
+            }
+        )
+
+    train_set, eval_set = split_data(emopia)
+    batch_size, patchilizer, model, scaler, is_autocast, optimizer = init()
+
+    train_set = DataLoader(
+        PatchilizedData(train_set, patchilizer),
+        batch_size=batch_size,
+        collate_fn=collate_batch,
+        shuffle=True,
+    )
+
+    eval_set = DataLoader(
+        PatchilizedData(eval_set, patchilizer),
+        batch_size=batch_size,
+        collate_fn=collate_batch,
+        shuffle=True,
+    )
+
+    lr_scheduler = get_scheduler(
+        name="cosine",
+        optimizer=optimizer,
+        num_warmup_steps=NUM_EPOCHS * len(train_set) / 10,
+        num_training_steps=NUM_EPOCHS * len(train_set),
+    )
+
+    if LOAD_FROM_CHECKPOINT:
+        if not os.path.exists(WEIGHT_PATH):
+            download()
+
+        checkpoint = torch.load(WEIGHT_PATH)
+
+        if torch.cuda.device_count() > 1:
+            model.module.load_state_dict(checkpoint["model"])
+        else:
+            model.load_state_dict(checkpoint["model"])
+
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        lr_scheduler.load_state_dict(checkpoint["lr_sched"])
+        pre_epoch = checkpoint["epoch"]
+        best_epoch = checkpoint["best_epoch"]
+        min_eval_loss = checkpoint["min_eval_loss"]
+        print("Successfully Loaded Checkpoint from Epoch %d" % pre_epoch)
+
+    else:
+        pre_epoch = 0
+        best_epoch = 0
+        min_eval_loss = 100
+
+    model = model.to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+    create_dir()
+
+    for epoch in range(1, NUM_EPOCHS + 1 - pre_epoch):
+        epoch += pre_epoch
+        print(f"{'-' * 21}Epoch {str(epoch)}{'-' * 21}")
+        train_loss = train_epoch(model, is_autocast, scaler)
+        eval_loss = eval_epoch(model)
+
+        with open(LOG_PATH, "a") as f:
+            f.write(
+                f"Epoch {str(epoch)}\ntrain_loss: {str(train_loss)}\neval_loss: {str(eval_loss)}\ntime: {time.asctime(time.localtime(time.time()))}\n\n"
+            )
+
+        if eval_loss < min_eval_loss:
+            best_epoch = epoch
+            min_eval_loss = eval_loss
+            if torch.cuda.device_count() > 1:
+                checkpoint = {
+                    "model": model.module.state_dict(),
+                    "optimizer": optimizer.state_dict(),
+                    "lr_sched": lr_scheduler.state_dict(),
+                    "epoch": epoch,
+                    "best_epoch": best_epoch,
+                    "min_eval_loss": min_eval_loss,
+                    "time_stamp": time.strftime(
+                        "%a_%d_%b_%Y_%H_%M_%S", time.localtime()
+                    ),
+                }
+
+            else:
+                checkpoint = {
+                    "model": model.state_dict(),
+                    "optimizer": optimizer.state_dict(),
+                    "lr_sched": lr_scheduler.state_dict(),
+                    "epoch": epoch,
+                    "best_epoch": best_epoch,
+                    "min_eval_loss": min_eval_loss,
+                    "time_stamp": time.strftime(
+                        "%a_%d_%b_%Y_%H_%M_%S", time.localtime()
+                    ),
+                }
+
+            torch.save(checkpoint, WEIGHT_PATH)
+
+    print(f"Best Eval Epoch : {str(best_epoch)}")
+    print(f"Min Eval Loss : {str(min_eval_loss)}")

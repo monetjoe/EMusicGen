baca123365fa1cb012598805bca8c21c6c154047
admin <admin@users.noreply.github.com>
2024-06-13 13:41:56 +0800

    recover 2 32

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ PATCH_NUM_LAYERS = 9  # Number of layers in the encoder
 CHAR_NUM_LAYERS = 3  # Number of layers in the decoder
 
 # Number of epochs to train for (if early stopping doesn't intervene)
-NUM_EPOCHS = 5  # 32
+NUM_EPOCHS = 32
 LEARNING_RATE = 5e-5  # Learning rate for the optimizer
 # Batch size for patch during training, 0 for full context
 PATCH_SAMPLING_BATCH_SIZE = 0
